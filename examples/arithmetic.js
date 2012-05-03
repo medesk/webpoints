f2e807697c605ce168c5bf2ab4640c4bbda09b4a
sakno <sakno@tncor.com>
2012-05-03 13:37:40 +1100

    Ensures and Requires contracts are completed

--- examples/arithmetic.js
+++ examples/arithmetic.js
@@ -1,5 +1,8 @@
 var WebPoints = require('../');
-var Application = WebPoints.Application, SyncHandler = WebPoints.SyncHandler, DefaultHelpProvider = WebPoints.helpProviders.DefaultHelpProvider;
+var Application = WebPoints.Application, 
+	SyncHandler = WebPoints.SyncHandler, 
+	DefaultHelpProvider = WebPoints.helpProviders.DefaultHelpProvider,
+	ContractHandler = WebPoints.ContractHandler;
 
 var app = new Application();
 
@@ -17,8 +20,19 @@ app.operations['/sub'] = {
 	handler: SyncHandler(function(x, y){ return x - y; })
 };
 
+app.operations['/div'] = {
+	method: 'get',
+	serialize: true,
+	params: {x: {deserialize: true}, y: {deserialize: true}},
+	handler: ContractHandler({
+		requires: function(x, y, callback){ callback(y != 0, 'Denominator cannot be zero.'); },
+		handler: function(x, y, callback){ callback(x / y); },
+		ensures: function(x, y, result, callback){ callback(!isNaN(result[0])); }
+	})
+}
+
 app.operations['/help'] = new DefaultHelpProvider(app.operations);
 
-console.info('This example demonstrates a simple RESTful service with three endpoints: /sum, /sub, /help.')
+console.info('This example demonstrates a simple RESTful service with three endpoints: /sum, /sub, /div, /help.')
 
 app.run(4000);
