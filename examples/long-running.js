c78d741e9ee39040fb4794ff040bc148d58f31ae
sakno <sakno@tncor.com>
2012-05-05 15:55:50 +1100

    Feature infrastructure and long-running tasks

---
+++ examples/long-running.js
@@ -0,0 +1,31 @@
+//This example demonstrates long-running tasks
+var WebPoints = require('../');
+var Application = WebPoints.Application,
+	TaskMonitorEndpoint = WebPoints.features.taskModel.TaskMonitorEndpoint,
+	TaskScheduler = WebPoints.features.taskModel.TaskScheduler;
+	
+var app = new Application(), scheduler = new TaskScheduler();
+
+//Enable long-running operations management
+app.features = [scheduler];
+
+app.operations['/someJob'] = {
+	method: 'get',
+	longRunning: true,			//this flag is used by TaskScheduler feature
+	params: {delay: {deserialize: true}},
+	handler: function(delay, callback){
+		//emulates hard work
+		setTimeout(function(){
+			//Returns result to the client.
+			callback('Hard job is completed!!!');	
+		}, delay);
+	}
+};
+
+//This trick allows to query task status from the browser
+TaskMonitorEndpoint.prototype.method = 'get';
+
+console.log('This example demonstrates HTTP long-running tasks. Query http://localhost:4000/someJob?delay=10000 from browser to get task ID.');
+console.log('Save the task ID returned by server and go to http://localhost:4000/tasks/[task-id] to obtain the result');
+
+app.run(4000);
